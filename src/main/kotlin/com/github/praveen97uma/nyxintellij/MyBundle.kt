cc9c36dc2c9f717f223df24031936d8268953de5
GitHub Action <action@github.com>
2025-08-12 18:10:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/praveen97uma/nyxintellij/MyBundle.kt
+++ src/main/kotlin/com/github/praveen97uma/nyxintellij/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.praveen97uma.nyxintellij
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/praveen97uma/nyxintellij/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/praveen97uma/nyxintellij/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.praveen97uma.nyxintellij.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
